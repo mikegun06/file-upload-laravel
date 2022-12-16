be4605b05d80efc09f272b1b6f0f997329b6cac0
Michael <michaelgunawan55@gmail.com>
2022-12-16 23:57:28 +0700

    fixing file

---
+++ file upload/resources/views/posts/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Edit Post') }}</div>
+
+                <div class="card-body">
+                    <!-- @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif -->
+                    
+                    <form action="" method="post">
+                        
+                        <div class="form-group">
+                            <label for="">Post Title</label>
+                            <input type="text" name="title" class="form-control" value="">
+                           
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Post Body</label>
+                            <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
+                            
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Publish At</label>
+                            <input type="date" name="published_at" class="form-control" value="">
+                            
+                        </div>
+
+                        
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ file upload/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [PostController::class, 'index'])->name('home');
+Route::get('posts/create', [PostController::class, 'create']);
+
+Route::get('posts/{post}/edit', [PostController::class, 'edit']);
+Route::get('posts/{post}', [PostController::class, 'show']);
+

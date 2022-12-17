8d6b54dd641ef02ac6a57f7becd1e04304c4de3d
Michael <michaelgunawan55@gmail.com>
2022-12-17 12:43:58 +0700

    update home

--- file upload/resources/views/master.blade.php
+++ file upload/resources/views/master.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar scroll</a>
+    <a class="navbar-brand" href="#">File Upload</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -20,7 +20,7 @@
         <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
+        {{-- <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item dropdown">
@@ -36,7 +36,7 @@
         </li>
         <li class="nav-item">
             <a class="nav-link disabled">Link</a>
-        </li>
+        </li> --}}
         </ul>
         <form class="d-flex">
         <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
